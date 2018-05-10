66d4d7501134fa80fac155e9def0eba3e4ae774f
ibrahem mustafa <ib.aldomyate@gmail.com>
2018-05-10 23:02:50 +0200

    make Connection to MongoDB

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/emle');
+let connection = process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost:27017/emle';
+mongoose.connect(connection);
 
 module.exports = {mongoose};
